aaf3cd66548a4b97e1c02c2585222862eb686478
Narumichi Kubo <narumichi7373@gmail.com>
2023-07-10 21:08:20 +0900

    Initial Commit

---
+++ AVFoundationSwiftUI/AVFoundationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVFoundationSwiftUIApp.swift
+//  AVFoundationSwiftUI
+//
+//  Created by Narumichi Kubo on 2023/07/10.
+//
+
+import SwiftUI
+
+@main
+struct AVFoundationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVFoundationSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AVFoundationSwiftUI
+//
+//  Created by Narumichi Kubo on 2023/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
